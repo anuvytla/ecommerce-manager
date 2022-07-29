99496e6c7217f819e4af2cdf20c47928a1fc6570
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-07-29 00:09:51 -0700

    Fix bugs in product delete and tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,7 +35,7 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
@@ -47,7 +47,7 @@ router.post('/', (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   // Calls the update method on the Tag model
-  Category.update(
+  Tag.update(
     {
       // All the fields you can update and the data attached to the request body.
       tag_name: req.body.tag_name,
